bcb4db16526872d67a5aa463f9dbcb673b210084
asus <123123@qq.com>
2020-03-24 10:46:57 +0800

    第一次用idea提交

---
+++ src/kyy/start.java
@@ -0,0 +1,12 @@
+package kyy;
+
+/**
+ * author : kyy
+ * time: 2020/3/24
+ * introduce:
+ */
+public class start {
+    public static void main(String[] args) {
+        System.out.println("aaaaaaaaa");
+    }
+}
